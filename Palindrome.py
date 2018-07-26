70e2a9318d678d1dd48f49695395c67fbcb62273
YaminiMahendran <yamini.mahendran1992@gmail.com>
2018-07-25 19:22:45 -0500

    Python

--- Palindrome.py
+++
@@ -1,8 +0,0 @@
-#Checking Palindrome of a String
-x=input("Enter a String:")
-y=x[::-1]
-print (y) 
-if(x==y):
-    print("String is a Palindrome")
-else:
-    print("String is not a Palindrome")
--- String_equavalence.py
+++
@@ -1,8 +0,0 @@
-string1=eval(input("Enter the first string:"))
-string2=eval(input("Enter the second string:"))
-string3=eval(input("Enter the third string:"))
-combined=string1+string2
-if(combined==string3):
-    print("They are equal")
-else:
-    print("They are not equal")
