436d1fda75354611e90701585c0cd7e0a23271dc
YaminiMahendran <yamini.mahendran1992@gmail.com>
2018-08-02 22:24:57 -0500

    Python/Leet

---
+++ reverse_vowels.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def reverseVowels(self, s):
+        vowels=['a','e','i','o','u','A','E','I','O','U']
+        new=[]
+        final=[]
+        revvowels=''
+        i=-1
+        for letter in s:
+            if letter in vowels:
+                new.append(letter)
+        for letter in s:
+            if letter not in vowels:
+                final.append(letter)
+            else:
+                final.append(new[i])
+                i -= 1
+        revvowels =''.join(final)
+        return revvowels
+
+s=Solution()
+print(s.reverseVowels('Leetcode'))
+        
