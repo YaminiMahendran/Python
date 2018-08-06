1f40fd89e79ebd1033c55bfa74f5179aeadc24ee
YaminiMahendran <yamini.mahendran1992@gmail.com>
2018-08-06 15:00:23 -0700

    Python/Leet

---
+++ Search_insert_position.py
@@ -0,0 +1,26 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        new=[]
+        for i,no in enumerate(nums):
+            if nums[i]==target:
+                return i
+            elif(max(nums)<target):
+                return len(nums)
+            else:
+                #print('p')
+                if nums[i]< target:
+                   pass
+                else:
+                    new.append(no)
+                    #print(3)
+            if len(new)!=0:
+                return nums.index(new[0])
+
+            
+                
+            
+        
+        
+        
+s=Solution()
+print(s.searchInsert([1,3,5,6],0))
