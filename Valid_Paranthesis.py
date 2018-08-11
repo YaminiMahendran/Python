9d5733eb312235584f7b6bfdb26f475bda46d95c
Yamini Mahendran <39605615+YaminiMahendran@users.noreply.github.com>
2018-08-10 22:20:22 -0700

    Python/Leet/Stack

---
+++ Valid_Paranthesis.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 10 17:22:05 2018
+
+@author: yamini
+
+"""
+class Solution:
+    def isValid(self, s):
+        my_list=[]
+        for each in s:
+            if(len(s)==1):
+                return False
+            if each=='(' or each=='[' or each =='{':
+                my_list.append(each)
+            elif each==')' or each==']' or each =='}':
+                if (len(my_list) !=0):
+                    popped=my_list.pop()
+                    print(popped)
+                    if popped =='(' and each !=')' or popped =='[' and each !=']' or popped =='{' and each !='}':
+                        return False
+                else:
+                    return False
+            else:
+                return False
+        if(len(my_list)==0):
+            return True
+        else:
+            return False
+    
+    
+    
+
+        
+
+s=Solution()
+print(s.isValid('{}[]()'))
