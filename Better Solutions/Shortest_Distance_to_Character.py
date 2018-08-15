b772d3bc5d6e3a44e2c5447e3c59b729d2c573df
Yamini Mahendran <39605615+YaminiMahendran@users.noreply.github.com>
2018-08-14 17:25:00 -0700

    Python/Leet

---
+++ Better Solutions/Shortest_Distance_to_Character.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 14 16:47:53 2018
+
+@author: yamini
+
+"""
+class Solution:
+    def shortestToChar(self, S, C):
+        new=[]
+        final=[]
+        for i,no in enumerate(S):
+            if no==C:
+                new.append(i)
+        for i in range(len(S)):
+            final.append(min([abs(j-i) for j in new]))
+        return final
+                 
+        
+        
+s=Solution()
+print(s.shortestToChar("loveleetcode",'e'))
