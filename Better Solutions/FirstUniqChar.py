f0781b773be213fbb785286c4e8533e205654916
Yamini Mahendran <39605615+YaminiMahendran@users.noreply.github.com>
2018-08-20 16:35:27 -0700

    Python/Leet

---
+++ Better Solutions/FirstUniqChar.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 20 16:20:14 2018
+
+@author: yamini
+
+"""
+class Solution:
+    def firstUniqChar(self, s):
+        dict1={}
+        for letter in s:
+            dict1[letter] =dict1.get(letter,0) + 1
+        for idx,ltr in enumerate(s):
+            if dict1[ltr] == 1:
+                return idx
+        return -1
+                
+            
+
+s=Solution()
+print(s.firstUniqChar("loveleetcode"))
