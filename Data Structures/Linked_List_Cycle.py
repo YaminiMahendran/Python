24716a1c79622cd9291c8587fe9d7fcf7b9a669a
Yamini Mahendran <39605615+YaminiMahendran@users.noreply.github.com>
2018-08-25 13:57:26 -0500

    Python/Leet/DS

--- Data Structures/Linked_List_Cycle.py
+++ Data Structures/Linked_List_Cycle.py
@@ -4,6 +4,9 @@ Created on Sat Aug 25 13:40:59 2018
 
 @author: yamini
 
+Logic:
+     Use slow and fast pointers and at one point both slow and fast will meet 
+     if its a cycle.
 """
 class Solution(object):
     def hasCycle(self, head):
