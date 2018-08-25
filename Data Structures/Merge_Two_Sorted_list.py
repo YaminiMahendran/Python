a3f0d934defd18a5804bddeb3938c01ac2575a9a
Yamini Mahendran <39605615+YaminiMahendran@users.noreply.github.com>
2018-08-24 19:07:11 -0500

    Python/Leet

---
+++ Data Structures/Merge_Two_Sorted_list.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 24 18:46:38 2018
+
+@author: yamini
+
+"""
+From Node import Node
+def mergeTwoLists1(self, l1, l2):
+    dummy = cur = Node(0)
+    while l1 and l2:
+        if l1.val < l2.val:
+            cur.next = l1
+            l1 = l1.next
+        else:
+            cur.next = l2
+            l2 = l2.next
+        cur = cur.next
+    cur.next = l1 or l2 # what does it do ?
+    return dummy.next
