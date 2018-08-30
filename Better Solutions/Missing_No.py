ae59549a4ffcb68861283bf33959eaf64d1de9a2
Yamini Mahendran <39605615+YaminiMahendran@users.noreply.github.com>
2018-08-30 18:14:34 -0500

    Python/Leet

--- Better Solutions/Missing_No.py
+++ Better Solutions/Missing_No.py
@@ -7,4 +7,9 @@ Created on Thu Aug 16 15:13:28 2018
 """
 class Solution:
     def missingNumber(self, nums):
+        
+        n = len(nums)
+        return n * (n + 1) / 2 - sum(nums)
+        """
         return sum(list(range(len(nums)+1))) - sum(nums)
+        """
