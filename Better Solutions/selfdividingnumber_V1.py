f3d63303279c9598e36d4c9e157fb7203d2f99e3
Yamini Mahendran <39605615+YaminiMahendran@users.noreply.github.com>
2018-08-31 09:21:26 -0500

    Python/Leet

--- Better Solutions/selfdividingnumber_V1.py
+++ Better Solutions/selfdividingnumber_V1.py
@@ -7,8 +7,24 @@ Created on Thu Aug  9 19:03:00 2018
 
 class Solution(object):
     def selfDividingNumbers(self, left, right):
-        isselfdividing=lambda num: '0' not in str(num) and all([num%int(x)==0 for x in str(num)])
-        return list(filter(isselfdividing,range(left,right+1)))
+        result =[]
+        for x in range(left,right+1):
+            if is_selfdividing(x):
+                result.append(x)
+        return result
+
+def is_selfdividing(x):
+    for no in str(x):
+        if no == "0" or int(x)%int(no)!=0:
+            return False
+    return True
+        
+"""
+isselfdividing=lambda num: '0' not in str(num) and all([num%int(x)==0 for x in str(num)])
+return list(filter(isselfdividing,range(left,right+1)))
+"""
+    
+
 
 s=Solution()
 print(s.selfDividingNumbers(1,22))
