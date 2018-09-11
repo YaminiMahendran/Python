270ba05b3d458e0d09d967f29376a9aaed8be1a0
Yamini Mahendran <39605615+YaminiMahendran@users.noreply.github.com>
2018-09-10 20:31:24 -0500

    Python/Leet

---
+++ Data Structures/Add_two_numbers.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 10 20:22:08 2018
+
+@author: yamini
+
+"""
+def addTwoNumbers(self, l1, l2):
+    carry = 0
+    root = n = ListNode(0)
+    while l1 or l2 or carry:
+        v1 = v2 = 0
+        if l1:
+            v1 = l1.val
+            l1 = l1.next
+        if l2:
+            v2 = l2.val
+            l2 = l2.next
+        carry, val = divmod(v1+v2+carry, 10)
+        n.next = ListNode(val)
+        n = n.next
+    return root.next
